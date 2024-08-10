dec816d519fd94c5a9b9b0ddf988ca4adea1a096
omar546565@gmail.com <omar546565@gmail.com>
2024-08-10 10:39:33 +0300

    sadas2

--- resources/js/layouts/FooterComponent.vue
+++ resources/js/layouts/FooterComponent.vue
@@ -5,7 +5,7 @@ const setting = JSON.parse(localStorage.getItem("setting"));
 <template>
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
-            <div class="copyright text-center my-auto">
+            <div v-if="setting" class="copyright text-center my-auto">
                 <span>Copyright &copy; {{ setting.name }} 2024</span>
             </div>
         </div>
