0c45c74b0a411aa5a3ee851365f7b0021641732e
omar546565@gmail.com <omar546565@gmail.com>
2024-08-10 11:02:53 +0300

    sadas2

--- database/seeders/PagesTableSeeder.php
+++ database/seeders/PagesTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class PagesTableSeeder extends Seeder
 {
@@ -12,71 +13,79 @@ class PagesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('pages')->insert(
+        $pages = [
+
             [
-            'id' => '1',
-            'page' => 'cpanel',
-            'icon' => 'fa fa-list-alt',
-            'path' => '#',
-            'order' => '1',
-            'page_id' => '0',
-        ],
-            [
-            'id' => '2',
-            'page' => 'pages',
-            'icon' => 'fa fa-file',
-            'path' => '/pages',
-            'order' => '3',
-            'page_id' => '1',
-        ],
-            [
-                'id' => '3',
-            'page' => 'users',
-            'icon' => 'fa fa-user-circle',
-            'path' => '/users',
-            'order' => '2',
-            'page_id' => '1',
-        ],
-            [
-                'id' => '4',
-            'page' => 'roles',
-            'icon' => 'fa fa-user-plus',
-            'path' => '/roles',
-            'order' => '3',
-            'page_id' => '1',
-        ],
-            [
-                'id' => '5',
-            'page' => 'permissions',
-            'icon' => 'fa fa-receipt',
-            'path' => '/permissions',
-            'order' => '5',
-            'page_id' => '1',
-        ],
-            [
-                'id' => '6',
-            'page' => 'translates',
-            'icon' => 'fa fa-language',
-            'path' => '/translates',
-            'order' => '7',
-            'page_id' => '5',
-        ],
-            [
-                'id' => '7',
-            'page' => 'GeneralSettings',
-            'icon' => 'fa fa-gears',
-            'path' => '/general-settings',
-            'order' => '1',
-            'page_id' => '1',
-        ],
-            [
-                'id' => '8',
-            'page' => 'appFonts',
-            'icon' => 'fa fa-font',
-            'path' => '/app-fonts',
-            'order' => '6',
-            'page_id' => '1',
-        ],
-    );
+                'id' => '1',
+                'page' => 'cpanel',
+                'icon' => 'fa fa-list-alt',
+                'path' => '#',
+                'order' => '1',
+                'page_id' => '0',
+            ],
+                [
+                'id' => '2',
+                'page' => 'pages',
+                'icon' => 'fa fa-file',
+                'path' => '/pages',
+                'order' => '3',
+                'page_id' => '1',
+            ],
+                [
+                    'id' => '3',
+                'page' => 'users',
+                'icon' => 'fa fa-user-circle',
+                'path' => '/users',
+                'order' => '2',
+                'page_id' => '1',
+            ],
+                [
+                    'id' => '4',
+                'page' => 'roles',
+                'icon' => 'fa fa-user-plus',
+                'path' => '/roles',
+                'order' => '3',
+                'page_id' => '1',
+            ],
+                [
+                    'id' => '5',
+                'page' => 'permissions',
+                'icon' => 'fa fa-receipt',
+                'path' => '/permissions',
+                'order' => '5',
+                'page_id' => '1',
+            ],
+                [
+                    'id' => '6',
+                'page' => 'translates',
+                'icon' => 'fa fa-language',
+                'path' => '/translates',
+                'order' => '7',
+                'page_id' => '5',
+            ],
+                [
+                    'id' => '7',
+                'page' => 'GeneralSettings',
+                'icon' => 'fa fa-gears',
+                'path' => '/general-settings',
+                'order' => '1',
+                'page_id' => '1',
+            ],
+                [
+                    'id' => '8',
+                'page' => 'appFonts',
+                'icon' => 'fa fa-font',
+                'path' => '/app-fonts',
+                'order' => '6',
+                'page_id' => '1',
+            ],
+            // Add more permissions as needed
+        ];
+
+        foreach ($pages as &$page) {
+            $page['created_at'] = Carbon::now();
+            $page['updated_at'] = Carbon::now();
+        }
+        DB::table('pages')->insert($pages);
     }
 }
