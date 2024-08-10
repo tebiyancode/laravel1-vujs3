4d43838964f71664af3d21db8ad343c5096a54d8
omar546565@gmail.com <omar546565@gmail.com>
2024-08-10 11:18:32 +0300

    sadas2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolesTableSeeder::class,
+            PagesTableSeeder::class,
             PermissionsTableSeeder::class,
             UsersTableSeeder::class,
-            PagesTableSeeder::class,
         ]);
     }
 }
